301b8a12e985ee8151c8e7563b1c539ccb44ead4
trixky <git config --global user.name>
2025-07-27 22:06:19 +0200

    dev: frontend > first commit (sveltekit + tailwind + daisyUi)

---
+++ frontend/src/lib/components/layout/Header.svelte
@@ -0,0 +1,47 @@
+<!-- --------------------------------------- SCRIPT -->
+<script lang="ts">
+import Icon from '@iconify/svelte';
+</script>
+
+<!-- --------------------------------------- CONTENT -->
+<header class="bg-base-100 w-full p-6 flex items-center justify-between">
+    <div class="flex items-center gap-8">
+        <h1>FlipPay</h1>
+        <nav class="-mb-4">
+            <ul class="flex items-center gap-6">
+                <li>
+                    <a href="/">
+                        <p>Trade</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/about">
+                        <p>Explore</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/contact">
+                        <p>Play</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="flex items-center gap-2">
+        <button class="btn btn-primary btn-md">
+            <Icon icon="mingcute:wallet-line" color="white" width="20" height="20" />
+            <span class="hidden md:block text-white">Connect Wallet</span>
+        </button>
+        <button class="btn btn-primary btn-ghost btn-md w-11 p-0">
+            <Icon icon="material-symbols:settings" color="white" width="20" height="20" />
+        </button>
+        <button class="btn btn-primary btn-ghost btn-md w-11 p-0">
+            <Icon icon="ix:about" color="white" width="20" height="20" />
+        </button>
+    </div>
+</header>
+
+<!-- --------------------------------------- STYLE -->
+<style>
+
+</style>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../style/app.css';
+	import Header from '$lib/components/layout/Header.svelte';
+	let { children } = $props();
+</script>
+
+<Header />
+<main class="w-full flex flex-col items-center justify-center p-6">
+	{@render children()}
+</main>
