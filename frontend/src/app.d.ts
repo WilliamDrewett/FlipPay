403b3a6d76f46b5787e11642b95bf033844f3cc8
trixky <git config --global user.name>
2025-07-29 17:27:29 +0200

    dev frontend env: try cloudflare

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -2,22 +2,12 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
-
-	// Ethereum window types for MetaMask and other wallets
-	interface Window {
-		ethereum?: {
-			request: (args: { method: string; params?: any[] }) => Promise<any>;
-			on: (event: string, callback: (...args: any[]) => void) => void;
-			removeListener: (event: string, callback: (...args: any[]) => void) => void;
-			isMetaMask?: boolean;
-		};
-	}
+        interface Platform {
+            env: Env
+            cf: CfProperties
+            ctx: ExecutionContext
+        }
+    }
 }
 
-export { };
+export {};
